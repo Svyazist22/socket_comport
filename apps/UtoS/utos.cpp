88c36ac2bf00306549f6178fb85b68d0307d38ce
Svyazist22 <bakanov.vp.22@gmail.com>
2021-07-13 11:50:18 +0300

    rename sybmols

--- apps/UtoS/utos.cpp
+++ apps/UtoS/utos.cpp
@@ -29,8 +29,8 @@ int main(int argc, char const *argv[])
     fifo_t fbuf; 
     uint8_t *fifo_buf = new uint8_t;
     
-    char *str = new char[1024]; // Команда с консоли
-    char *buf = new char[1024]; // Сообщение с компорта
+    char *str_cons = new char[1024]; // Команда с консоли
+    char *str_com = new char[1024]; // Сообщение с компорта
     char *h1 = new char[17];  // Хэш отправленного сообщения
     char *h2 = new char[17]; // Хэш полученного сообщения
 
@@ -41,19 +41,20 @@ int main(int argc, char const *argv[])
     while (1)
     {
         printf("Write command:");
-        std::cin.getline(str,1024);     // Считываем команду с консоли
+        std::cin.getline(str_cons,1024);     // Считываем команду с консоли
+        
         // Проверка на непустое сообщение
-        if (strlen(str)==0)
+        if (strlen(str_cons)==0)
         {
             log.err("Command input error (the command is empty). Please try again");
             continue;
         }
         
-        cl.client_write(str);           // Отправляем на компорт
-        get_hash(str,strlen(str),h1);   // Получаем хэш отправленного сообщения
+        cl.client_write(str_cons);           // Отправляем на компорт
+        get_hash(str_cons,strlen(str_cons),h1);   // Получаем хэш отправленного сообщения
 
         // Команда остановки программ
-        if (strcmp(str,"stop")==0)
+        if (strcmp(str_cons,"stop")==0)
         {
             cl.client_stop();
             log.err("The program is stopped!");
@@ -63,9 +64,9 @@ int main(int argc, char const *argv[])
         sleep(1);                       // Т.к. используется один компорт нужна задержка
      
         uart.uart_receive(fd,&fbuf);    // Слушаем компорт и записываем в буффер
-        fifo_read_pop(&fbuf,buf,1024);  // Берем из буффера полученное сообщение
-        log.info("Message received from UART:%s",buf);
-        get_hash(buf,strlen(buf),h2);   // Получаем хэш полученного сообщения
+        fifo_read_pop(&fbuf,str_com,1024);  // Берем из буффера полученное сообщение
+        log.info("Message received from UART:%s",str_com);
+        get_hash(str_com,strlen(str_com),h2);   // Получаем хэш полученного сообщения
         
         // Сравниваем хэши сообщений
         if(compare_hash(h1,h2))         
